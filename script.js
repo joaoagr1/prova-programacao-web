28659ae352f20378517a70fe40f214f16774fb1e
joaoagr1 <125477822+joaoagr1@users.noreply.github.com>
2024-04-24 22:48:43 +0000

    commit2

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("Esta é a Página 3.");
+window.addEventListener("load", (event) => {
+    fetchEstados()
+  });
+
+function fetchEstados() {
+
+    const apiUrl = `https://servicodados.ibge.gov.br/api/v1/localidades/estados`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const estadosLista = document.getElementById('estados-lista');
+            console.log(estadosLista)
+           // estadosLista.innerHTML = 'ok';
+            data.forEach(estado => {
+
+                const row = document.createElement('li');
+                row.addEventListener('click', function () {
+                                });
+                row.innerHTML = `
+                      <li>${estado.nome}</li>
+                `;
+                estadosLista.appendChild(row);
+            });
+        });
+}
