3a6110c3eadf849a918f04fef2282c4defdda04d
joaoagr1 <125477822+joaoagr1@users.noreply.github.com>
2024-04-25 01:33:26 +0000

    commitFinalMesmo

--- favoritos/script.js
+++ favoritos/script.js
@@ -1,7 +1,59 @@
-favoritos = localStorage.getItem("favoritos")
+function splitString(stringToSplit, separator) {
+    const arrayOfStrings = stringToSplit.split(separator);
+  
+    
+    console.log(arrayOfStrings)
 
-favoritos.forEach((favorito) => {
+
+
+    const favoritosLista = document.getElementById('favoritos-lista');
+    arrayOfStrings.forEach(municipio => {
+
+        
+
+        const row = document.createElement('li');
+
+
+        
+        row.innerHTML = `
+              <li>${municipio}</li>
+        `;
+        favoritosLista.appendChild(row);
 
 
 
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+  })
+}
+
+  let favoritos = []
+
+favoritos.push(localStorage.getItem("favoritos"))
+
+const comma = ",";
+
+splitString(localStorage.getItem("favoritos"), comma);
+
